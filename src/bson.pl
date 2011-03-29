0c270a08f0a644d88a2d7bf6e75cf32f293de0ec
khueue <khueue@gmail.com>
2011-03-29 15:51:01 +0200

    Decode {hello: 32}.

--- src/bson.pl
+++ src/bson.pl
@@ -6,12 +6,19 @@
 
 test_decode :-
     % \x16\x00\x00\x00\x02hello\x00\x06\x00\x00\x00world\x00\x00
-    Bson = [
+    _BsonHelloWorld = [
         0x16,0x00,0x00,0x00,0x02,
         104, 101, 108, 108, 111,
         0x00,0x06,0x00,0x00,0x00,
         119, 111, 114, 108, 100,
         0x00,0x00],
+    BsonHello32 = [
+        0xFF,0x00,0x00,0x00,
+        0x10, 104, 101, 108, 108, 111, 0x00,
+        0x20,0x00,0x00,0x00,
+        0x00
+    ],
+    Bson = BsonHello32,
     bson:decode(Bson, Term),
     io:format('BSON: ~w~n', [Bson]),
     io:format('Term: ~w~n', [Term]).
@@ -30,6 +37,53 @@ document(bson(Elements)) -->
     length(_Length),
     element_list(Elements),
     end.
+
+element_list([Element|Elements]) -->
+    element(Element), !,
+    element_list(Elements).
+element_list([]) --> [].
+
+element(Element) -->
+    [0x02], !,
+    element_utf8_string(Element).
+element(Element) -->
+    [0x10], !,
+    element_int32(Element).
+
+element_utf8_string((Ename,String)) -->
+    e_name(Ename),
+    string(String).
+
+element_int32((Ename,Integer)) -->
+    e_name(Ename),
+    int32(Integer).
+
+% XXX: Handle unicode (do not use cstring).
+string(String) -->
+    length(_Integer),
+    cstring(AsciiList),
+    { atom_asciilist(String, AsciiList) }.
+
+length(Length) -->
+    int32(Length).
+
+int32(Integer) -->
+    [Byte0,Byte1,Byte2,Byte3],
+    { bytes_to_integer(Byte0, Byte1, Byte2, Byte3, Integer) }.
+
+e_name(Ename) -->
+    cstring(AsciiList),
+    { atom_asciilist(Ename, AsciiList) }.
+
+cstring([]) --> [0x00], !.
+cstring([Char|Cs]) -->
+    [Char],
+    cstring(Cs).
+
+end --> [0x00].
+
+atom_asciilist(Atom, AsciiList) :-
+    name(Atom, AsciiList).
 
 bytes_to_integer(Byte0, Byte1, Byte2, Byte3, Integer) :-
     Integer is
@@ -50,41 +104,3 @@ bytes_to_integer(
         (Byte5 << (8*5)) +
         (Byte6 << (8*6)) +
         (Byte7 << (8*7)).
-
-element_list([Element|Elements]) -->
-    element(Element),
-    !,
-    element_list(Elements).
-element_list([]) --> [].
-
-element(Element) -->
-    [0x02],
-    element_utf8_string(Element).
-
-element_utf8_string((Ename,String)) -->
-    e_name(Ename),
-    string(String).
-
-% XXX: Handle unicode (do not use cstring).
-string(String) -->
-    length(_Integer),
-    cstring(AsciiList),
-    { atom_asciilist(String, AsciiList) }.
-
-length(Length) -->
-    [Byte0,Byte1,Byte2,Byte3],
-    { bytes_to_integer(Byte0, Byte1, Byte2, Byte3, Length) }.
-
-e_name(Ename) -->
-    cstring(AsciiList),
-    { atom_asciilist(Ename, AsciiList) }.
-
-cstring([]) --> [0x00], !.
-cstring([Char|Cs]) -->
-    [Char],
-    cstring(Cs).
-
-end --> [0x00].
-
-atom_asciilist(Atom, AsciiList) :-
-    name(Atom, AsciiList).
