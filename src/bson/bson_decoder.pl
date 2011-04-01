d18e68bdcea3b25ebfe3422f1bd9f79a0714873b
khueue <khueue@gmail.com>
2011-04-01 14:42:27 +0200

    Better documented tests.

--- src/bson/bson_decoder.pl
+++ src/bson/bson_decoder.pl
@@ -10,44 +10,42 @@
 
 :- begin_tests(bson_decoder).
 
-test('hello: world', [true(Got == Expected)]) :-
+test('valid utf8', [true(Got == Expected)]) :-
     Bson =
     [
-        0x16,0x00,0x00,0x00,0x02,
-        104, 101, 108, 108, 111,
-        0x00,0x06,0x00,0x00,0x00,
-        119, 111, 114, 108, 100,
-        0x00,0x00
-    ],
-    Expected =
-    [
-        'hello': 'world'
-    ],
-    bson_decoder:decode(Bson, Got).
-
-test('utf8', [true(Got == Expected)]) :-
-    Bson =
-    [
-        0xFF,0,0,0, % Doc length.
+        xxx_not_impl,0,0,0, % Length of top doc.
         0x02, % String tag.
             0xc3,0xa4, 0, % Ename, "ä\0".
+            0x06,0,0,0, % String's byte length, incl. nul.
+            0xc3,0xa4, 0, 0xc3,0xa4, 0, % String data, "ä\0ä\0".
+        0 % End of top doc.
+    ],
+    Expected =
+    [
+        'ä': 'ä\0ä'
+    ],
+    bson_decoder:decode(Bson, Got).
+
+test('nuls not allowed in ename', [throws(bson_error(_))]) :-
+    Bson =
+    [
+        xxx_not_impl,0,0,0, % Length of top doc.
+        0x02, % String tag.
+            0xc3,0xa4, 0, 0xc3,0xa4, 0, % Ename, "ä\0ä\0".
             0x03,0,0,0, % String's byte length, incl. nul.
             0xc3,0xa4, 0, % String data, "ä\0".
-        0 % Doc end.
+        0 % End of top doc.
     ],
-    Expected =
-    [
-        'ä': 'ä'
-    ],
-    bson_decoder:decode(Bson, Got).
+    bson_decoder:decode(Bson, _Got).
 
-test('hello: 32', [true(Got == Expected)]) :-
+test('int32', [true(Got == Expected)]) :-
     Bson =
     [
-        0xFF,0x00,0x00,0x00,
-        0x10, 104, 101, 108, 108, 111, 0x00,
-        0x20,0x00,0x00,0x00,
-        0x00
+        xxx_not_impl,0,0,0, % Length of top doc.
+        0x10, % Int32 tag
+            104,101,108,108,111, 0, % Ename "hello\0".
+            0x20,0,0,0, % Int32 data, 32.
+        0 % End of top doc.
     ],
     Expected =
     [
@@ -55,14 +53,14 @@ test('hello: 32', [true(Got == Expected)]) :-
     ],
     bson_decoder:decode(Bson, Got).
 
-test('hello: 32, 64-bit', [true(Got == Expected)]) :-
+test('int64', [true(Got == Expected)]) :-
     Bson =
     [
-        0xFF,0x00,0x00,0x00,
-        0x12,
-        104, 101, 108, 108, 111, 0x00,
-        0x20,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
-        0x00
+        xxx_not_impl,0,0,0, % Length of top doc.
+        0x12, % Int64 tag
+            104,101,108,108,111, 0, % Ename "hello\0".
+            0x20,0,0,0, 0,0,0,0, % Int64 data, 32.
+        0 % End of top doc.
     ],
     Expected =
     [
@@ -70,14 +68,14 @@ test('hello: 32, 64-bit', [true(Got == Expected)]) :-
     ],
     bson_decoder:decode(Bson, Got).
 
-test('hello: 5.05', [true(Got == Expected)]) :-
+test('float', [true(Got == Expected)]) :-
     Bson =
     [
-        0xFF,0x00,0x00,0x00, % Top doc length.
+        xxx_not_impl,0,0,0, % Length of top doc.
         0x01, % Double tag.
-        104, 101, 108, 108, 111, 0x00, % Ename "hello\0".
-        51,51,51,51,51,51,20,64, % Double data 5.05.
-        0x00
+            104,101,108,108,111, 0, % Ename "hello\0".
+            51,51,51,51, 51,51,20,64, % Double data, 5.05.
+        0 % End of top doc.
     ],
     Expected =
     [
@@ -85,25 +83,25 @@ test('hello: 5.05', [true(Got == Expected)]) :-
     ],
     bson_decoder:decode(Bson, Got).
 
-test('complex', [true(Got == Expected)]) :-
+test('embedded array', [true(Got == Expected)]) :-
     Bson =
     [
-        49,0,0,0, % length
-        0x04, % array tag
-            66,83,79,78,0, % element name, "BSON\0"
-            38,0,0,0, % length of embedded doc (array)
-            0x02, % string tag
-                48,0, % index 0 ("0\0")
-                8,0,0,0, % length of string, incl. nul
-                97,119,101,115,111,109,101, 0, % string, "awesome\0"
-            0x01, % double tag
-                49,0, % ename, index 1 ("1\0")
-                51,51,51,51,51,51,20,64, % double 8-byte
-            0x10, % int32 tag
-                50,0, % ename, index 2 ("2\0")
-                194,7,0,0, % int32 data (1986)
-            0, % end of array doc
-        0 % end of doc
+        49,0,0,0, % Length of top doc.
+        0x04, % Array tag.
+            66,83,79,78,0, % Ename "BSON\0".
+            38,0,0,0, % Length of embedded doc (array).
+            0x02, % String tag.
+                48,0, % Ename, index 0 ("0\0").
+                8,0,0,0, % String's byte length, incl. nul.
+                97,119,101,115,111,109,101, 0, % String data, "awesome\0".
+            0x01, % Double tag.
+                49,0, % Ename, index 1 ("1\0").
+                51,51,51,51,51,51,20,64, % Double 8-byte data, 5.05.
+            0x10, % Int32 tag.
+                50,0, % Ename, index 2 ("2\0").
+                194,7,0,0, % Int32 data, 1986.
+            0, % End of embedded doc (array).
+        0 % End of top doc.
     ],
     Expected =
     [
@@ -171,7 +169,7 @@ element(Element) -->
 
 element_document(Pair) -->
     key_name(Ename),
-    document(Doc),
+    value_document(Doc),
     { key_value_pair(Ename, Doc, Pair) }.
 
 element_double(Pair) -->
@@ -196,51 +194,25 @@ element_int64(Pair) -->
 
 key_value_pair(Key, Value, Key:Value).
 
+value_document(Doc) -->
+    document(Doc).
+
 value_string(String) -->
     length(Length),
     value_utf8_string(ByteList, Length),
     { bytes_to_code_points(ByteList, String) }.
 
-% A bit of a hack, but in order to interpret raw bytes as UTF-8
-% we use a memory file as a temporary buffer, fill it with the
-% bytes and then read them back, interpreting them as UTF-8.
-% See: <http://www.swi-prolog.org/pldoc/doc_for?object=memory_file_to_atom/3>
-bytes_to_code_points(Bytes, Utf8Atom) :-
-    new_memory_file(MemFile),
-    write_bytes_to_memory_file(Bytes, MemFile),
-    memory_file_to_codes(MemFile, CodePoints, utf8),
-    free_memory_file(MemFile),
-    atom_codes(Utf8Atom, CodePoints).
-
-write_bytes_to_memory_file(Bytes, MemFile) :-
-    open_memory_file(MemFile, write, Stream, [encoding(octet)]),
-    put_bytes(Bytes, Stream),
-    close(Stream).
-
-put_bytes([], _Stream).
-put_bytes([Byte|Bs], Stream) :-
-    put_byte(Stream, Byte),
-    put_bytes(Bs, Stream).
-
-/*
-% Old (shorter) version using more atom construction. XXX Benchmark.
-bytes_to_code_points(Bytes, Utf8Atom) :-
-    atom_chars(RawAtom, Bytes),
-    atom_to_memory_file(RawAtom, MemFile),
-    memory_file_to_codes(MemFile, CodePoints, utf8),
-    free_memory_file(MemFile),
-    atom_codes(Utf8Atom, CodePoints).
-*/
-
 value_utf8_string(CharList, Length) -->
     { LengthMinusNul is Length - 1 },
     value_utf8_string(CharList, 0, LengthMinusNul).
 
-value_utf8_string([], Length, Length) --> [0x00], !.
 value_utf8_string([Byte|Bs], Length0, Length) -->
-    [Byte],
+    { Length0 < Length },
+    !,
+    [Byte], % May be nul.
     { Length1 is Length0 + 1 },
     value_utf8_string(Bs, Length1, Length).
+value_utf8_string([], Length, Length) --> [0x00].
 
 value_double(Double) -->
     double(Double).
@@ -266,6 +238,37 @@ int64(Integer) -->
 key_name(Ename) -->
     cstring(CharList),
     { bytes_to_code_points(CharList, Ename) }.
+
+% A bit of a hack, but in order to interpret raw bytes as UTF-8
+% we use a memory file as a temporary buffer, fill it with the
+% bytes and then read them back, interpreting them as UTF-8.
+% See: <http://www.swi-prolog.org/pldoc/doc_for?object=memory_file_to_atom/3>
+bytes_to_code_points(Bytes, Utf8Atom) :-
+    new_memory_file(MemFile),
+    bytes_to_memory_file(Bytes, MemFile),
+    memory_file_to_codes(MemFile, CodePoints, utf8),
+    free_memory_file(MemFile),
+    atom_codes(Utf8Atom, CodePoints).
+
+bytes_to_memory_file(Bytes, MemFile) :-
+    open_memory_file(MemFile, write, Stream, [encoding(octet)]),
+    put_bytes(Bytes, Stream),
+    close(Stream).
+
+put_bytes([], _Stream).
+put_bytes([Byte|Bs], Stream) :-
+    put_byte(Stream, Byte),
+    put_bytes(Bs, Stream).
+
+/*
+% Old (shorter) version using more atom construction. XXX Benchmark.
+bytes_to_code_points(Bytes, Utf8Atom) :-
+    atom_chars(RawAtom, Bytes),
+    atom_to_memory_file(RawAtom, MemFile),
+    memory_file_to_codes(MemFile, CodePoints, utf8),
+    free_memory_file(MemFile),
+    atom_codes(Utf8Atom, CodePoints).
+*/
 
 length(Length) -->
     int32(Length).
