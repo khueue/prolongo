3f3d972a1dc7e7cd0354f8a493fef58c8be97e2b
khueue <khueue@gmail.com>
2011-04-01 23:31:05 +0200

    Comments.

--- load.pl
+++ load.pl
@@ -11,7 +11,7 @@
 % Enable this to see how modules and such are located.
 % :- set_prolog_flag(verbose_file_search, true).
 
-% Set up load paths.
+% Set up paths for module loading.
 :-
     prolog_load_context(directory, RootDir), % Dir of this file.
     atom_concat(RootDir, '/src/bson', Bson),
