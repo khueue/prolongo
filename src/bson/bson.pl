e5d06633c2796518948629dafeb9d83daa375722
khueue <khueue@gmail.com>
2011-04-04 12:50:42 +0200

    Add term_bson relation to bson.

--- src/bson/bson.pl
+++ src/bson/bson.pl
@@ -1,8 +1,30 @@
 %%% Implements BSON 1.0. <http://bsonspec.org/>
 
-:- module(_, []).
+:- module(_,
+    [
+        term_bson/2
+    ]).
 
-:- reexport(bson_decoder, [decode/2]).
-:- reexport(bson_encoder, [encode/2]).
+:- use_module(bson_decoder, []).
+:- use_module(bson_encoder, []).
 
 :- include(misc(common)).
+
+%%  term_bson(+Term:list, ?Bson:list(bytes)) is semidet.
+%%  term_bson(?Term:list, +Bson:list(bytes)) is semidet.
+%
+%   True if XXX ...
+
+term_bson(Term, Bson) :-
+    nonvar(Term),
+    nonvar(Bson),
+    !,
+    bson_decoder:decode(Bson, Term). % XXX Go with the fastest one.
+term_bson(Term, Bson) :-
+    nonvar(Bson),
+    !,
+    bson_decoder:decode(Bson, Term).
+term_bson(Term, Bson) :-
+    nonvar(Term),
+    !,
+    bson_encoder:encode(Term, Bson).
