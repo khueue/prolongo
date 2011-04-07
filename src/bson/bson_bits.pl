e92433a73192a8f26f9781b7a8fc7d169245fa37
khueue <khueue@gmail.com>
2011-04-07 14:40:25 +0200

    Move more bit-handling to bson_bits.

--- src/bson/bson_bits.pl
+++ src/bson/bson_bits.pl
@@ -8,7 +8,7 @@
 
 % <module> Low-level bytes-to-number conversions.
 %
-% Implemented in C.
+% Some predicates are implemented in C.
 
 :- use_foreign_library(foreign(bson_bits)).
 
@@ -43,3 +43,43 @@
 %   bytes (0..255) B0..B7 interpreted as a 64-bit little-endian integer.
 %
 %   Implemented in foreign library.
+
+%%  fits_in_32_bits
+%
+%   XXX
+
+fits_in_32_bits(Int) :-
+    -(2**(32-1)) =< Int, Int =< (2**(32-1))-1.
+
+%%  fits_in_64_bits
+%
+%   XXX
+
+fits_in_64_bits(Int) :-
+    -(2**(64-1)) =< Int, Int =< (2**(64-1))-1.
+
+%%  int32_to_bytes
+%
+%   XXX
+
+% Todo: What happens when given a too large (unbounded) integer?
+int32_to_bytes(Int32, B0, B1, B2, B3) :-
+    B0 is (Int32 >> (0*8)) /\ 0xFF,
+    B1 is (Int32 >> (1*8)) /\ 0xFF,
+    B2 is (Int32 >> (2*8)) /\ 0xFF,
+    B3 is (Int32 >> (3*8)) /\ 0xFF.
+
+%%  int64_to_bytes
+%
+%   XXX
+
+% Todo: What happens when given a too large (unbounded) integer?
+int64_to_bytes(Int64, B0, B1, B2, B3, B4, B5, B6, B7) :-
+    B0 is (Int64 >> (0*8)) /\ 0xFF,
+    B1 is (Int64 >> (1*8)) /\ 0xFF,
+    B2 is (Int64 >> (2*8)) /\ 0xFF,
+    B3 is (Int64 >> (3*8)) /\ 0xFF,
+    B4 is (Int64 >> (4*8)) /\ 0xFF,
+    B5 is (Int64 >> (5*8)) /\ 0xFF,
+    B6 is (Int64 >> (6*8)) /\ 0xFF,
+    B7 is (Int64 >> (7*8)) /\ 0xFF.
--- src/bson/bson_encoder.pl
+++ src/bson/bson_encoder.pl
@@ -25,7 +25,7 @@ document(Elements, Len) -->
     elements(Elements, LenElements),
     [0],
     { Len is 4 + LenElements + 1 },
-    { int32_to_bytes(Len, L0, L1, L2, L3) }.
+    { bson_bits:int32_to_bytes(Len, L0, L1, L2, L3) }.
 
 elements(Elements, Len) -->
     elements(Elements, 0, Len).
@@ -59,10 +59,10 @@ value(Value, Tag, Len) -->
     { builtin:integer(Value) },
     !,
     value_integer(Value, Tag, Len).
-value(Value, 0x01, Len) -->
+value(Value, Tag, Len) -->
     { builtin:float(Value) },
     !,
-    value_double(Value, Len).
+    value_float(Value, Tag, Len).
 value(Value, Tag, Len) -->
     { builtin:atom(Value) },
     !,
@@ -87,28 +87,28 @@ add_array_keys([Value|Values], IndexInt, [Key:Value|Pairs]) :-
     IndexInt1 is IndexInt + 1,
     add_array_keys(Values, IndexInt1, Pairs).
 
-value_double(Float, 8) -->
+value_float(Float, 0x01, 8) -->
     { bson_bits:float_to_bytes(Float, B0, B1, B2, B3, B4, B5, B6, B7) },
     [B0,B1,B2,B3,B4,B5,B6,B7].
 
 value_integer(Integer, 0x10, 4) -->
-    { fits_in_32_bits(Integer) },
+    { bson_bits:fits_in_32_bits(Integer) },
     !,
     value_int32(Integer).
 value_integer(Integer, 0x12, 8) -->
-    { fits_in_64_bits(Integer) },
+    { bson_bits:fits_in_64_bits(Integer) },
     !,
     value_int64(Integer).
 
 value_int32(Integer) -->
-    { int32_to_bytes(Integer, B0, B1, B2, B3) },
+    { bson_bits:int32_to_bytes(Integer, B0, B1, B2, B3) },
     [B0,B1,B2,B3].
 
 value_int64(Integer) -->
-    { int64_to_bytes(Integer, B0, B1, B2, B3, B4, B5, B6, B7) },
+    { bson_bits:int64_to_bytes(Integer, B0, B1, B2, B3, B4, B5, B6, B7) },
     [B0,B1,B2,B3,B4,B5,B6,B7].
 
-value_atom(undefined, 0x06, 0)   --> [].
+value_atom(undefined, 0x06, 0)   --> []. % Deprecated in BSON 1.0.
 value_atom(false,     0x08, 1)   --> [0].
 value_atom(true,      0x08, 1)   --> [1].
 value_atom(null,      0x0A, 0)   --> [].
@@ -121,32 +121,8 @@ value_string(Text, Len) -->
     { bson_unicode:utf8_bytes(Text, Bytes) },
     { lists:length(Bytes, StrLen) },
     { StrLenNul is StrLen + 1 },
-    { int32_to_bytes(StrLenNul, L0, L1, L2, L3) },
+    { bson_bits:int32_to_bytes(StrLenNul, L0, L1, L2, L3) },
     { Len is 4 + StrLenNul },
     [L0,L1,L2,L3],
     Bytes,
     [0].
-
-fits_in_32_bits(Int) :-
-    -(2**(32-1)) =< Int, Int =< (2**(32-1))-1.
-
-fits_in_64_bits(Int) :-
-    -(2**(64-1)) =< Int, Int =< (2**(64-1))-1.
-
-% Todo: What happens when given a too large (unbounded) integer?
-int32_to_bytes(Int32, B0, B1, B2, B3) :-
-    B0 is (Int32 >> (0*8)) /\ 0xFF,
-    B1 is (Int32 >> (1*8)) /\ 0xFF,
-    B2 is (Int32 >> (2*8)) /\ 0xFF,
-    B3 is (Int32 >> (3*8)) /\ 0xFF.
-
-% Todo: What happens when given a too large (unbounded) integer?
-int64_to_bytes(Int64, B0, B1, B2, B3, B4, B5, B6, B7) :-
-    B0 is (Int64 >> (0*8)) /\ 0xFF,
-    B1 is (Int64 >> (1*8)) /\ 0xFF,
-    B2 is (Int64 >> (2*8)) /\ 0xFF,
-    B3 is (Int64 >> (3*8)) /\ 0xFF,
-    B4 is (Int64 >> (4*8)) /\ 0xFF,
-    B5 is (Int64 >> (5*8)) /\ 0xFF,
-    B6 is (Int64 >> (6*8)) /\ 0xFF,
-    B7 is (Int64 >> (7*8)) /\ 0xFF.
