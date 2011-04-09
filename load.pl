01b771c4dde1d83fd2b179e9155d1c49c9256dc4
khueue <khueue@gmail.com>
2011-04-09 17:46:26 +0200

    Cleanup.

--- load.pl
+++ load.pl
@@ -39,13 +39,10 @@ test :-
 gen_doc :-
     load_all_modules,
     use_module(library(doc_latex)),
-    doc_latex(
-        bson(bson),
-        'doc/bson.tex',
-        []).
+    doc_latex(bson(bson), 'doc/bson.tex', []).
 
 load_all_modules :-
-    use_module(library(pldoc)),
+    use_module(library(pldoc)), % Load first.
     use_module(bson(bson), []).
 
 load_all_tests :-
